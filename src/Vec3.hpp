cadaaf35fe8261ae4c679803982474d8322444da
youse0297 <youse0297@gmail.com>
2026-07-18 15:47:38 +0800

    Create Vec3.hpp

---
+++ src/Vec3.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+
+class Vec3 {
+    public:
+        double x, y, z;
+
+        //构造函数
+        Vec3(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
+
+        //加法
+        Vec3 operator+(const Vec3& other) const;
+        Vec3& operator+=(const Vec3& other);
+
+        //减法
+        Vec3 operator-(const Vec3& other) const;
+        Vec3& operator-=(const Vec3& other);
+
+        //数乘（标量乘法）
+        Vec3 operator*(double scalar) const;
+        friend Vec3 operator*(double scalar, const Vec3& vec);
+        Vec3& operator*=(double scalar);
+
+        //数除（标量除法,可选）
+        Vec3 operator/(double scalar) const;
+        friend Vec3 operator/(double scalar, const Vec3& vec);
+        Vec3& operator/=(double scalar);
+
+        //取负
+        Vec3 operator-() const;
+
+        //点积
+        double dot(const Vec3& other) const;
+        double length() const;
+        double lengthSquared() const;
+
+        //归一化
+        Vec3 normalized() const;
+        //原地归一化
+        Vec3& normalize();
+
+        // 三维叉积，返回一个新的 Vec3 向量
+        Vec3 cross(const Vec3& other) const;
+
+        //打印
+        friend std::ostream& operator<<(std::ostream& os, const Vec3& v);
+};
