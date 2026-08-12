f9dafd2cad81c516d251ed74c1d1059a9ecfabd0
youse0297 <youse0297@gmail.com>
2026-08-13 03:39:47 +0800

    Create TA_MaterialFunctions.hlsl

---
+++ UnityMaterialLab/Assets/_TA/ShaderGraph/Library/TA_MaterialFunctions.hlsl
@@ -0,0 +1,10 @@
+//UNITY_SHADER_NO_UPGRADE
+#ifndef TA_MATERIAL_FUNCTION_LIBRARY_INCLUDED
+#define TA_MATERIAL_FUNCTION_LIBRARY_INCLUDED
+
+#include "TA_MaterialUV.hlsl"
+#include "TA_MaterialNormal.hlsl"
+#include "TA_MaterialChannels.hlsl"
+#include "TA_MaterialColor.hlsl"
+
+#endif
